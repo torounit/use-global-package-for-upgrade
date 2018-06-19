9b36fdbb4dbd1401422579cca05902b45c47288c
Toro_Unit (Hiroshi Urabe) <mail@torounit.com>
2018-06-19 18:34:01 +0900

    init

---
+++ use-global-package-for-upgrade.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     Use Global Package For Upgrade
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          YOUR NAME HERE
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     use-global-package-for-upgrade
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Use_Global_Package_For_Upgrade
+ */
+
+// Your code starts here.
